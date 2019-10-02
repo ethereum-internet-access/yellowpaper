ba6e9d251a8571ee68d8abbaf0b0510ceb5b8b40
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-10-02 19:59:51 +0200

    Add connection flow picture

--- images/yellowize.pl
+++ images/yellowize.pl
@@ -8,6 +8,7 @@ print "Yellowizing $inputDir\n";
 
 foreach my $file (<$inputDir*.png>) {
   my ($filename) = $file =~ m/\/([^\/]+)\.png\Z/;
+  next if ($filename =~ m/-y\Z/);
   my $cmd = "convert -fuzz 0% -fill 'rgb(255, 250, 230)' -opaque 'rgb(255, 255, 255)' $file $inputDir${filename}-y.png\n";
   print "$cmd\n";
   system($cmd);
