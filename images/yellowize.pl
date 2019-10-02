329a3e347ebd8d1384840e1e8df28b585848d68a
Egio, Alfonso <alfonso.egio@i2cat.net>
2019-10-02 17:17:32 +0200

    Change title

--- images/yellowize.pl
+++ images/yellowize.pl
@@ -11,4 +11,6 @@ foreach my $file (<$inputDir*.png>) {
   my $cmd = "convert -fuzz 0% -fill 'rgb(255, 250, 230)' -opaque 'rgb(255, 255, 255)' $file $inputDir${filename}-y.png\n";
   print "$cmd\n";
   system($cmd);
+  my $cmd2 = "convert $inputDir${filename}-y.png $inputDir${filename}-y.eps";
+  system($cmd2);
 }
